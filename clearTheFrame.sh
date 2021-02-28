25fad6e0bca23ba908d6cf944f9c0856b35c992e
ThaLindil <63932761+ThaLindil@users.noreply.github.com>
2021-02-28 09:20:36 +0100

    Update clearTheFrame.sh

--- clearTheFrame.sh
+++ clearTheFrame.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ls -ltr  /mnt/images/ | grep ".jpg" | head -n-30 |  awk '{print $9}' | mv `xarg$
+# Default the last 30 images will stay and the older one will be moved to the folder backup. If you want another number of image change the -n-30.
+ls -ltr  /mnt/images/ | grep ".jpg" | head -n-30 |  awk '{print $9}' | mv `xarg$ backup
 
